69d5f8a6924e5ab08e1aab88a05a6cfe3ca0199b
Nico Aßfalg <nico.assfalg@student.kit.edu>
2021-04-25 13:08:19 +0200

    Bunch of Fixes

--- init-servers.sh
+++ init-servers.sh
@@ -9,5 +9,5 @@ curl "${ldbbc_url}" -X DELETE -L
 
 echo "--> Filling LDP..."
 find ontologies -name '*.ttl' -exec curl "${ldbbc_url}/" -X PUT -Hcontent-type:text/turtle -T {} -L \; -print
-find data       -name '*.ttl' -exec curl "${ldbbc_url}/" -X PUT -Hcontent-type:text/turtle -T {} -L \; -print
-#find data       -name '*.ttl' -exec curl "${ldbbc_url}"  -X POST -Hcontent-type:text/turtle -T {} -L \; -print
+find data       -name 'flamethrower*.ttl' -exec curl "${ldbbc_url}/" -X PUT -Hcontent-type:text/turtle -T {} -L \; -print
+#find rules      -name '*.n3' -exec curl "${ldbbc_url}/"  -X PUT -Hcontent-type:application/n3 -T {} -L \; -print
